6f8060875819fa50a693c164211524dddd932b65
alah35 <maxnet663@gmail.com>
2022-03-12 15:45:28 +0300

    Task 4: algorithm is optimized

--- Tasks/Task4.cpp
+++ Tasks/Task4.cpp
@@ -19,7 +19,19 @@ void show_by_abs(T *arr, int len) {
 }
 
 void task4() {
-    int arr[6] = {-100,-50, -5, 1, 10, 15};
-    outputArray(arr,sizeof(arr));
-    show_by_abs(arr, sizeof(arr));
+    std::vector<int> v = {-100, -50, -1, 1, 2, 3, 4};
+
+    int j = 0;
+    while (j < v.size() && v[j] < 0) {
+        ++j;
+    }
+
+    int i = j--;
+    while (j >= 0 || i < v.size()) {
+        int res = (j >= 0 && (i == v.size() || v[i] > -v[j]) ? v[j--] : v[i++]);
+        std::cout << res << ' ';
+    }
+
+    std::cout << '\n';
+
 }
